0f82edf03da1c82d8b348cf18b1db55169a2c65f
kastastin <kostya242421@gmail.com>
2022-11-18 15:10:58 +0200

    setCoordinates, setBounds, useEffect

--- src/App.js
+++ src/App.js
@@ -11,13 +11,23 @@ import Map from './components/Map/Map'
 const App = () => {
   const [places, setPlaces] = useState([]);
 
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState(null);
+
   useEffect(() => {
-    getPlacesData()
+    navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude} }) => {
+      setCoordinates({ lat: latitude, lng: longitude });
+    });
+  }, []);
+
+  useEffect(() => {
+    if (bounds) {
+      getPlacesData(bounds.sw, bounds.ne)
       .then((data) => {
-        console.log(data);
         setPlaces(data);
       })
-  }, []);
+    }
+  }, [coordinates, bounds]);
 
   return (
     <>
@@ -25,10 +35,14 @@ const App = () => {
       <Header />
       <Grid container spacing={3} style={{ width: '100%' }}>
         <Grid item xs={12} md={4}>
-          <List />
+          <List places={places} />
         </Grid>
         <Grid item xs={12} md={8}>
-          <Map />
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+          />
         </Grid>
       </Grid>
     </>
--- src/api/index.js
+++ src/api/index.js
@@ -2,22 +2,20 @@ import axios from 'axios';
 
 const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
 
-const options = {
-  params: {
-    bl_latitude: '11.847676',
-    tr_latitude: '12.838442',
-    bl_longitude: '109.095887',
-    tr_longitude: '109.149359',
-  },
-  headers: {
-    'X-RapidAPI-Key': '5d07a04db8mshca2effab91e5cd4p1250e3jsn00f126e30a0c',
-    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-  }
-};
-
-export const getPlacesData = async () => {
+export const getPlacesData = async (sw, ne) => {
   try {
-    const { data: {data} } = await axios.get(URL, options);
+    const { data: {data} } = await axios.get(URL, {
+      params: {
+        bl_latitude: sw.lat,
+        bl_longitude: sw.lng,
+        tr_latitude: ne.lat,
+        tr_longitude: ne.lng,
+      },
+      headers: {
+        'X-RapidAPI-Key': '5d07a04db8mshca2effab91e5cd4p1250e3jsn00f126e30a0c',
+        'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+      }
+    });
 
     return data;
   } catch (error) {
